bd2e888380157e22486051e67165608bb52929f6
Assela Pathirana <assela@pathirana.net>
2022-05-23 12:57:15 +0200

    Parameterize mid point

--- gumbel.py
+++ gumbel.py
@@ -33,48 +33,50 @@ def linfit(df,xcol,ycol):
     regr.fit(X_, Y_)
     return regr.coef_[0][0], regr.intercept_[0]
 
-ser_=pd.read_csv('data.SHIA.csv', sep=',') # you may want to change the seperatore. 
-ser=ser_[:10]
-
-ser=ser.sort_values('maxvalue', ascending=False).reset_index(drop=True)
-
-
+def plot(ser):
 #https://serc.carleton.edu/hydromodules/steps/166250.html
 #Gringorten's plotting position https://glossary.ametsoc.org/wiki/Gringorten_plotting_position
 # qi=(i=a)/(N+1-2a) a= constant for estimation=0.44 using Gringorten's method
 # p=(1-qi)
 # Tr=1/qi
-NY=len(ser)
-ser['qi'] = (ser.index.values+1-.44)/(NY+1-2*.44)
-ser['p'] = 1-ser['qi']
-ser['llp']=-np.log(-np.log(ser['p']))
-xticklabels=[2,3,5,10,20,50]
-xtl=[x for x in xticklabels]
-xticks=[-np.log(-np.log(1-1/x)) for x in xtl]
+    NY=len(ser)
+    ser['qi'] = (ser.index.values+1-.44)/(NY+1-2*.44)
+    ser['p'] = 1-ser['qi']
+    ser['llp']=-np.log(-np.log(ser['p']))
+    xticklabels=[2,3,5,10,20,50]
+    xtl=[x for x in xticklabels]
+    xticks=[-np.log(-np.log(1-1/x)) for x in xtl]
 
-fig, ax = plt.subplots(figsize=(10, 5))
+    fig, ax = plt.subplots(figsize=(10, 5))
 #xlim = [0,8] 
 #ax.set_xlim(xlim)    
-g=sns.regplot(x='llp',y='maxvalue',data=ser,fit_reg=True, ax=ax) 
-sns.regplot(x='llp', y='maxvalue', data=ser, fit_reg=False, ax=ax)
-g.set(xticklabels=xticklabels)
-g.set(xticks=xticks)
-g.set_title("Extreme Value Fit")
-g.set_xlabel("Return period (years)")
-g.set_ylabel("Maximum rainfall (mm/day)")
+    g=sns.regplot(x='llp',y='maxvalue',data=ser,fit_reg=True, ax=ax) 
+    sns.regplot(x='llp', y='maxvalue', data=ser, fit_reg=False, ax=ax)
+    g.set(xticklabels=xticklabels)
+    g.set(xticks=xticks)
+    g.set_title("Extreme Value Fit")
+    g.set_xlabel("Return period (years)")
+    g.set_ylabel("Maximum rainfall (mm/day)")
 
 # rolling average
 #ser_rol=ser.rolling(ROLL, win_type='triang').mean()
 #sns.regplot(x='llp', y='maxvalue', data=ser_rol, fit_reg=False, ax=ax)
 
-m,c=linfit(ser,'llp','maxvalue')
-props = dict(boxstyle='round', alpha=0.5,color=sns.color_palette()[0])
-textstr = r'$y={:4f}(-ln(-ln(1-1/(Tr))+{:4f} $'.format(m, c)
-g.text(0.1, 0.9, textstr, transform=ax.transAxes, fontsize=14, bbox=props)
+    m,c=linfit(ser,'llp','maxvalue')
+    props = dict(boxstyle='round', alpha=0.5,color=sns.color_palette()[0])
+    textstr = r'$y={:4f}(-ln(-ln(1-1/(Tr))+{:4f} $'.format(m, c)
+    g.text(0.1, 0.9, textstr, transform=ax.transAxes, fontsize=14, bbox=props)
 
-print(ser)
 
-plt.show()
+
+MID=10 # middle point (For 20 years, this should be 10, then it will take two windows 0..9 and 10..19)
+ser_=pd.read_csv('data.SHIA.csv', sep=',') # you may want to change the seperatore. 
+for fr,to in [[0,MID],[MID,-1]]:
+    ser=ser_[fr:to]
+    print(ser)
+    ser=ser.sort_values('maxvalue', ascending=False).reset_index(drop=True)
+    plot(ser)
+    plt.show()
 
 
 
